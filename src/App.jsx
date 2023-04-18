4fc657cee3a526632dc62ccc0f14a04cdf528578
Miliyonayalew <miliayalew@gmail.com>
2023-04-19 01:47:57 +0300

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Article from "./pages/Article"
 import ArticleList from "./pages/ArticleList"
+import Navbar from "./components/Navbar"
 
 const App = () => {
 
   return (
     <BrowserRouter>
+       <Navbar />
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
