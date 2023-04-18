ca3c317d2a217c16ad202b383fc0cc843d8fd4a1
Miliyonayalew <miliayalew@gmail.com>
2023-04-19 01:32:47 +0300

    Setup the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Article from "./pages/Article"
+import ArticleList from "./pages/ArticleList"
+
 const App = () => {
 
   return (
-    <div className="bg-gray-900 text-center text-white">MERN</div>
+    <BrowserRouter>
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/article" element={<Article />} />
+          <Route path="/articleList" element={<ArticleList />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
       )
 }
 
